8e96ea897a4256ecce24dcfad3ac2b26c0526d7a
karinaG343 <83309631+karinaG343@users.noreply.github.com>
2022-11-11 11:11:11 -0600

    Proyecto finalizado con mas imagenes

--- imagenes.js
+++ imagenes.js
@@ -3,12 +3,38 @@ let reverso = "img/reverso.png";
 
 function cargarImagenes() {
 
-	halloween[0] = "img/anabell.jpg";
-	halloween[1] = "img/bruja.jpg";
-	halloween[2] = "img/catrina.jpg";
-	halloween[3] = "img/chuky.jpg";
-	halloween[4] = "img/it.jpg";
-	halloween[5] = "img/krueger.jpg";
-	halloween[6] = "img/myers.jpg";
-	halloween[7] = "img/saw.jpg";
+	halloween[0] = "img/addams.jpg";
+	halloween[1] = "img/anabell.jpg";
+	halloween[2] = "img/bitelchus.jpg";
+	halloween[3] = "img/bruja.jpg";
+	halloween[4] = "img/catrina.jpg";
+	halloween[5] = "img/caza.jpg";
+	halloween[6] = "img/chuky.jpg";
+	halloween[7] = "img/coraline.jpg";
+	halloween[8] = "img/dracula.jpg";
+	halloween[9] = "img/evil.jpg";
+	halloween[10] = "img/ghost.jpg";
+	halloween[11] = "img/helboy.jpg";
+	halloween[12] = "img/hocus.jpg";
+	halloween[13] = "img/huerfana.jpg";
+	halloween[14] = "img/it.jpg";
+	halloween[15] = "img/jack.jpg";
+	halloween[16] = "img/krueger.jpg";
+	halloween[17] = "img/lobo.jpg";
+	halloween[18] = "img/monja.jpg";
+	halloween[19] = "img/mundo.jpg";
+	halloween[20] = "img/myers.jpg";
+	halloween[21] = "img/nino.jpg";
+	halloween[22] = "img/novia.jpg";
+	halloween[23] = "img/novio.jpg";
+	halloween[24] = "img/pajaro.jpg";
+	halloween[25] = "img/papel.jpg";
+	halloween[26] = "img/payaso.jpg";
+	halloween[27] = "img/purga.jpg";
+	halloween[28] = "img/purga2.jpg";
+	halloween[29] = "img/saw.jpg";
+	halloween[30] = "img/scream.jpg";
+	halloween[31] = "img/sombras.jpg";
+	halloween[32] = "img/tijeras.jpg";
+	halloween[33] = "img/zombie.jpg";
 }
